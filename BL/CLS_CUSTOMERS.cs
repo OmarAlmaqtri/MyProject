2c9808a95686226f01012686c02cc8b9bc6dd7e9
OmarAlmaqtri <42122578+OmarAlmaqtri@users.noreply.github.com>
2018-12-20 04:41:25 +0300

    Add files via upload

---
+++ BL/CLS_CUSTOMERS.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Final_Project.BL
+{
+    class CLS_CUSTOMERS
+    {
+        public void ADD_CUSTOMER(string First_Name, string Last_Name, string Tel,
+                        string Email, byte[] Picture, string Criterion , string Cust_name , string Cust_pass)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DAL.Open();
+            SqlParameter[] param = new SqlParameter[8];
+            param[0] = new SqlParameter("@First_Name", SqlDbType.VarChar, 25);
+            param[0].Value = First_Name;
+
+            param[1] = new SqlParameter("@Last_Name", SqlDbType.VarChar, 25);
+            param[1].Value = Last_Name;
+
+            param[2] = new SqlParameter("@Tel", SqlDbType.VarChar, 15);
+            param[2].Value = Tel;
+
+            param[3] = new SqlParameter("@Email", SqlDbType.VarChar, 25);
+            param[3].Value = Email;
+
+            param[4] = new SqlParameter("@Picture", SqlDbType.Image);
+            param[4].Value = Picture;
+
+            param[5] = new SqlParameter("@Criterion", SqlDbType.VarChar, 50);
+            param[5].Value = Criterion;
+
+            param[6] = new SqlParameter("@Cust_name", SqlDbType.VarChar, 50);
+            param[6].Value = Cust_name;
+
+            param[7] = new SqlParameter("@Cust_pass", SqlDbType.VarChar, 50);
+            param[7].Value = Cust_pass;
+
+            DAL.ExecuteCommand("ADD_CUSTOMER", param);
+            DAL.Close();
+        }
+
+        public void EDIT_CUSTOMER(string First_Name, string Last_Name, string Tel,
+                        string Email, byte[] Picture, string Criterion, int ID, string Cust_name, string Cust_pass)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DAL.Open();
+            SqlParameter[] param = new SqlParameter[9];
+            param[0] = new SqlParameter("@First_Name", SqlDbType.VarChar, 25);
+            param[0].Value = First_Name;
+
+            param[1] = new SqlParameter("@Last_Name", SqlDbType.VarChar, 25);
+            param[1].Value = Last_Name;
+
+            param[2] = new SqlParameter("@Tel", SqlDbType.VarChar, 15);
+            param[2].Value = Tel;
+
+            param[3] = new SqlParameter("@Email", SqlDbType.VarChar, 25);
+            param[3].Value = Email;
+
+            param[4] = new SqlParameter("@Picture", SqlDbType.Image);
+            param[4].Value = Picture;
+
+            param[5] = new SqlParameter("@Criterion", SqlDbType.VarChar, 50);
+            param[5].Value = Criterion;
+
+            param[6] = new SqlParameter("@ID", SqlDbType.Int);
+            param[6].Value = ID;
+
+            param[7] = new SqlParameter("@Cust_name", SqlDbType.VarChar, 50);
+            param[7].Value = Cust_name;
+
+            param[8] = new SqlParameter("@Cust_pass", SqlDbType.VarChar, 50);
+            param[8].Value = Cust_pass;
+
+            DAL.ExecuteCommand("EDIT_CUSTOMER", param);
+            DAL.Close();
+        }
+
+        public void DELETE_CUSTOMER(int ID)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DAL.Open();
+            SqlParameter[] param = new SqlParameter[1];
+
+            param[0] = new SqlParameter("@ID", SqlDbType.Int);
+            param[0].Value = ID;
+
+
+            DAL.ExecuteCommand("DELETE_CUSTOMER", param);
+            DAL.Close();
+        }
+
+        public DataTable GET_ALL_CUSTOMERS()
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            Dt = DAL.SelectData("GET_ALL_CUSTOMERS", null);
+            DAL.Close();
+            return Dt;
+        }
+
+        public DataTable Search_CUSTOMER(string critetion)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@criterion", SqlDbType.VarChar, 50);
+            param[0].Value = critetion;
+            Dt = DAL.SelectData("Search_CUSTOMER", param);
+            DAL.Close();
+            return Dt;
+        }
+    }
+}
---
+++ BL/CLS_PRODUCTS.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Final_Project.BL
+{
+    class CLS_PRODUCTS
+    {
+        public DataTable GET_ALL_CATEGORIES()
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            DataTable Dt1 = new DataTable();
+            Dt = DAL.SelectData("GET_ALL_CATEGORIES", null);
+            DAL.Close();
+            return Dt;
+        }
+            
+        public void ADD_PRUDUCT(int ID_cat, string label_product, string ID_product,
+                                int Qte, string Price, byte[] img, string photo_path)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DAL.Open();
+            SqlParameter[] param = new SqlParameter[7];
+            param[0] = new SqlParameter("@ID_CAT", SqlDbType.Int);
+            param[0].Value = ID_cat;
+
+            param[1] = new SqlParameter("@ID_PRODUCT", SqlDbType.VarChar, 30);
+            param[1].Value = ID_product;
+
+            param[2] = new SqlParameter("@Lable", SqlDbType.VarChar, 30);
+            param[2].Value = label_product;
+
+            param[3] = new SqlParameter("@Qte", SqlDbType.Int);
+            param[3].Value = Qte;
+
+            param[4] = new SqlParameter("@PRICE", SqlDbType.VarChar, 50);
+            param[4].Value = Price;
+
+            param[5] = new SqlParameter("@Img", SqlDbType.Image);
+            param[5].Value = img;
+
+            param[6] = new SqlParameter("@IMAGE_FOR_WEB", SqlDbType.VarChar, 250);
+            param[6].Value = photo_path;
+            DAL.ExecuteCommand("ADD_PRODUCT", param);
+            DAL.Close();
+            
+        }
+
+        public void UPDATE_PRODUCT(int ID_cat, string label_product, string ID_product,
+                                int Qte, string Price, byte[] img, string photo_path)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DAL.Open();
+            SqlParameter[] param = new SqlParameter[7];
+            param[0] = new SqlParameter("@ID_CAT", SqlDbType.Int);
+            param[0].Value = ID_cat;
+
+            param[1] = new SqlParameter("@ID_PRODUCT", SqlDbType.VarChar, 30);
+            param[1].Value = ID_product;
+
+            param[2] = new SqlParameter("@Lable", SqlDbType.VarChar, 30);
+            param[2].Value = label_product;
+
+            param[3] = new SqlParameter("@Qte", SqlDbType.Int);
+            param[3].Value = Qte;
+
+            param[4] = new SqlParameter("@PRICE", SqlDbType.VarChar, 50);
+            param[4].Value = Price;
+
+            param[5] = new SqlParameter("@Img", SqlDbType.Image);
+            param[5].Value = img;
+
+            param[6] = new SqlParameter("@IMAGE_FOR_WEB", SqlDbType.VarChar, 250);
+            param[6].Value = photo_path;
+
+            DAL.ExecuteCommand("UPDATE_PRODUCT", param);
+            DAL.Close();
+            
+        }
+
+        public DataTable VerifyProductID(string ID)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            param[0].Value = ID;
+            Dt = DAL.SelectData("VerifyProductID", param);
+            DAL.Close();
+            return Dt;
+        }
+
+        public DataTable GET_ALL_PRODUCTS()
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            Dt = DAL.SelectData("GET_ALL_PRODUCTS", null);
+            DAL.Close();
+            return Dt;
+        }
+        public DataTable GET_ALL_PRODUCTS_LESSTHAN()
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            Dt = DAL.SelectData("GET_ALL_PRODUCTS_LESSTHAN", null);
+            DAL.Close();
+            return Dt;
+        }
+        public DataTable SearchProduct(string ID)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            param[0].Value = ID;
+            Dt = DAL.SelectData("SearchProduct", param);
+            DAL.Close();
+            return Dt;
+        }
+        public DataTable SearchProductLessThen(string ID)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            param[0].Value = ID;
+            Dt = DAL.SelectData("SearchProduct", param);
+            DAL.Close();
+            return Dt;
+        }
+
+        public DataTable GET_IMAGE_PRODUCT(string ID)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DataTable Dt = new DataTable();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            param[0].Value = ID;
+            Dt = DAL.SelectData("GET_IMAGE_PRODUCT", param);
+            DAL.Close();
+            return Dt;
+        }
+        public void DeleteProduct(string ID)
+        {
+            DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+            DAL.Open();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            param[0].Value = ID;
+            DAL.ExecuteCommand("DeleteProduct", param);
+            DAL.Close();
+        }
+
+    }
+}
