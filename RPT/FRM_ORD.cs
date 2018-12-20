99b9a4aa5c0580e4665fe6250075ba3c84e53007
OmarAlmaqtri <42122578+OmarAlmaqtri@users.noreply.github.com>
2018-12-20 04:58:07 +0300

    Add files via upload

---
+++ RPT/FRM_ORD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Final_Project.RPT
+{
+    public partial class FRM_ORD : Form
+    {
+        public FRM_ORD()
+        {
+            InitializeComponent();
+        }
+    }
+}
